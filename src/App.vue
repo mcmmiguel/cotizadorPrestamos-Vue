50b028eb9a27a121f808063dff4d064ffa966eed
Miguel Cobian <miguelcobian99@gmail.com>
2024-02-01 20:40:39 -0600

    Feat. Modify state to show the amount

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <script setup>
-import Header from './components/Header.vue';
+  import {ref} from 'vue';
+  import Header from './components/Header.vue';
+
+  const cantidad = ref(0);
+
+  function handleChange(e) {
+    cantidad.value = Number(e.target.value);
+  }
 </script>
 
 <template>
   <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
     <Header />
+
+    <div class="my-5">
+      <input 
+        @input="handleChange" 
+        class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600" 
+        type="range">
+      {{ cantidad }}
+    </div>
   </div>
 </template>
 
