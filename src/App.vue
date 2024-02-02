156a39a11f708be0d0c228b5faad094cfc02ae34
Miguel Cobian <miguelcobian99@gmail.com>
2024-02-01 19:50:56 -0600

    Chore. Set up tailwind

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,7 @@
 </script>
 
 <template>
-  <h1>Hola Mundo</h1>
+  <h1 class="text-4xl">Hola Mundo</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
