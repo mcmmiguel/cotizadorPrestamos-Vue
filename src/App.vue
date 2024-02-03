cce0ef8f41fbb117e9d73cdbb85fa0746c33dcaf
Miguel Cobian <miguelcobian99@gmail.com>
2024-02-02 18:56:20 -0600

    Feat. Compute property to format the amount

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import {ref} from 'vue';
+  import {ref, computed} from 'vue';
   import Header from './components/Header.vue';
 
   const minCantidad = 0;
@@ -7,6 +7,16 @@
   const stepCantidad = 100;
 
   const cantidad = ref(10000);
+
+  const formatearDinero = computed( () => {
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+
+    return formatter.format(cantidad.value)
+  });
+
 </script>
 
 <template>
@@ -22,7 +32,7 @@
         :step="stepCantidad"
         v-model.number="cantidad"
         >
-      {{ cantidad }}
+      <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">{{formatearDinero}}</p>
     </div>
   </div>
 </template>
